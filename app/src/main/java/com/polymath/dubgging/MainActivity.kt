aa68de3c61fe0ff315f14789ae0d0dcded1bfa09
Priyanshu Jain <priyanshujain2341@gmail.com>
2022-01-07 21:43:51 +0530

    Added an exception which causes the app to crash

--- app/src/main/java/com/polymath/dubgging/MainActivity.kt
+++ app/src/main/java/com/polymath/dubgging/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.polymath.dubgging
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        logging()
+        division()
+    }
+
+    private fun division() {
+        var numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Log.v(TAG, "The value is ${numerator / denominator}")
+            denominator--
+        }
+    }
+
+    private fun logging() {
+        Log.v(TAG, "This is my first log")
     }
 }
